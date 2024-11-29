be87290d2a0154c948b321cde8031e5ca00c1b19
Lotte <lotte.karlsson@tuni.fi>
2024-11-29 22:08:21 +0200

    Succesful sharing of volume for password. Authentication for API works

--- state_api/api_gateway.py
+++ state_api/api_gateway.py
@@ -3,55 +3,81 @@ import datetime
 import docker
 import requests
 import logging
+import base64
+from passlib.apache import HtpasswdFile
 
 app = Flask(__name__)
 
+#Initialise variables to store the state and log
 creation_time = f"State initialized at {datetime.datetime.now()}"
 trueState = "INIT"
 log = [creation_time]
 
-
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+#Get the htpasswd file
+htpasswd = HtpasswdFile('/etc/nginx/.htpasswd')
 
+#Get the credentials from the request
+def get_credentials():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_type, auth_info = auth_header.split(None, 1)
+        if auth_type.lower() == 'basic':
+            username, password = base64.b64decode(auth_info).decode('utf-8').split(':', 1)
+            return username, password
+    return None, None
+#Check if the credentials are correct
+def verify_credentials(username, password):
+    if htpasswd.check_password(username, password):
+        return True
+    return False
 
-#dummy responses to test pipeline
-@app.route('/stateGet', methods=['GET'])
+#Getting the state
+@app.route('/state', methods=['GET'])
 def get_state():
     return trueState, 200
 
+#Changing the state
 @app.route('/state', methods=['PUT'])
 def set_state():
-    global trueState    
+    global trueState
+    global log
+
+    #When the state is INIT, the user must provide credentials
+    if trueState == "INIT":
+        if not request.headers.get('Authorization'):
+            return "No Authorization was provided. System in INIT state", 401
+        username, password = get_credentials()
+        if not verify_credentials(username, password):
+            return "Unauthorized, wrong credentials", 401  
+    #Searching for containers    
     service2Container = ""
     nginxContainer = ""
-    
     client = docker.from_env()
     containers = client.containers.list()
-
     for container in containers:
         if 'service2' in container.name:
             service2Container = container
         if 'nginx' in container.name:
             nginxContainer = container
 
+    #If no containers, return error
     if not service2Container:
         logging.error("Service2 container not found")
         return "Service2 container not found", 500
     if not nginxContainer:
         logging.error("Nginx container not found")
         return "Nginx Container not found", 500
-    
     logging.info(f"nginx container: {service2Container}")
 
+    #Getting the state from the request, and checking if same
     state = request.data.decode("utf-8").strip('"')
     logging.info(f"State: {state} (type: {type(state)})")
     logging.info(f"Previous state: {trueState} (type: {type(trueState)})")
     if trueState == state:
         logging.info("State request same as current state")
         return state, 200
-
-    global log
 
     #Valid states
     logging.info(state not in ["PAUSED", "SHUTDOWN", "INIT", "RUNNING"])
@@ -60,6 +86,7 @@ def set_state():
     
     client: docker.DockerClient = docker.from_env()
     
+    #Based on the state, the containers are paused, unpaused, restarted or shutdown
     match state:
         case "PAUSED":
                 service2Container.pause();
@@ -77,12 +104,14 @@ def set_state():
         case "RUNNING":
                 if trueState != "INIT":
                     service2Container.unpause();
-    log.append(f"State changed to {state} from {trueState} at {datetime.datetime.now()}\n")
+    #Adding to the log and changing the state if all went well
+    log.append(f"State changed to {state} from {trueState} at {datetime.datetime.now()}")
     logging.info(f"State changed to {state} from {trueState}")
     trueState = state
 
     return trueState, 200
 
+#Getting info from services
 @app.route('/request', methods=['GET'])
 def handle_request():
     try:
@@ -92,7 +121,7 @@ def handle_request():
     except:
         return "Error", 500
        
-
+#Getting the log
 @app.route('/run-log', methods=['GET'])
 def get_run_log():
     return str(log), 200
