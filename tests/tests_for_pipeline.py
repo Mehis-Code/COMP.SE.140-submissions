975023090bda62838512bba988f5d55b298645d4
Lotte <lotte.karlsson@tuni.fi>
2024-11-29 22:22:26 +0200

    Fixing tests

--- tests/tests_for_pipeline.py
+++ tests/tests_for_pipeline.py
@@ -1,12 +1,22 @@
 import requests
-
+from requests.auth import HTTPBasicAuth
 #API url
 #Automated tests for the pipeline
 BASE_URL = "http://docker:8197"
-def test_set_state(param):
-    response = requests.put(f"{BASE_URL}/state", data=param)
+
+def test_set_state(param, username, password):
+    headers = {
+        'Content-Type': 'text/plain'
+    }
+    response = requests.put(
+        f"{BASE_URL}/state",
+        data=param,
+        headers=headers,
+        auth=HTTPBasicAuth(username, password)
+    )
     assert response.status_code == 200, "Failed to set state"
     assert response.text == param, "Failed to set state"
+
 
 def test_get_state():
     response = requests.get(f"{BASE_URL}/state")
@@ -24,13 +34,11 @@ def test_get_run_log():
 
 if __name__ == "__main__":
     print("Running tests")
-    test_set_state("PAUSED")
-    test_set_state("INIT")
-    test_get_state("RUNNING")
-
+    test_set_state("PAUSED", "user1", "devops")
+    test_get_state("RUNNING", "user1", "devops")
 
     test_handle_request()
     test_get_run_log()
-
-    test_set_state("SHUTDOWN")
+    test_set_state("INIT", "user1", "devops")
+    test_set_state("SHUTDOWN", "user1", "devops")
     print("All tests passed");
